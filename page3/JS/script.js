b9cfcbc7397b4e1380972b639673bef781fe89c8
Luan de Souza Xavier <luandesouzaxavier@outlook.com.br>
2023-09-18 04:31:58 -0300

    teste

---
+++ page3/JS/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const preencherFormulario = (endereco) =>{
+    document.getElementById('logradouro').value = endereco.logradouro
+}
+
+
+const pesquisaCep =async () =>{
+    const cep = document.getElementById('cep').value
+    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    const dados = await fetch(url);
+    const endereco = await dados.json();
+    preencherFormulario(endereco)
+
+    
+}
+
+document.getElementById('cep')
+.addEventListener('focusout', pesquisaCep)
